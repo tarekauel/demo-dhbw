c8cae3c05e9c8994d597ef6716ec2f8045e3a8d0
Tarek Auel <tarek@auel.io>
2016-08-30 19:27:11 +0200

    eq fn

--- test.lib.js
+++ test.lib.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 var fib = require('./lib.js');
+var eq = require('./eq.js');
 
 describe('Fib', function() {
   it('should return 1 for fib(1)', function() {
@@ -8,4 +9,10 @@ describe('Fib', function() {
   it('should return 2 for fib(3)', function() {
     assert.equal(2, fib(3));
   });
+});
+
+describe('Eq', function() {
+  it('1 should be 1', function() {
+    assert.equal(true, eq(1,1));
+  })
 });
